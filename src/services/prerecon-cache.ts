0674fa6ccde4ccf9e666c814b4d1dd85a1870e7a
Josep Maria Viñolas Auquer <josepmaria@isardvdi.com>
2026-03-09 01:47:27 +0100

    fix: update copyright header for contributed file

--- src/services/prerecon-cache.ts
+++ src/services/prerecon-cache.ts
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 Keygraph, Inc.
+// Copyright (C) 2026 Josep Maria Viñolas Auquer <josepmaria@isardvdi.com>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License version 3
